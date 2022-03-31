09f2a2f34262cd7e49fd78a377dd26936e7af426
a-baade <andreasbaade@hotmail.com>
2022-03-31 20:36:24 +0200

    Initial commit

---
+++ client/addNewMovie.jsx
@@ -0,0 +1,33 @@
+import React, {useState} from "react";
+
+function FormInput({label, value, onChangeValue}) {
+    return <div>
+        <label>
+            <strong>{label}</strong>{" "}
+            <input value={value} onChange={(e) => onChangeValue(e.target.value)} />
+        </label>
+    </div>;
+}
+
+export function AddNewMovie({ createMovie }) {
+    const [title, setTitle] = useState("");
+    const [year, setYear] = useState("");
+    const [country, setCountry] = useState("");
+    const [plot, setPlot] = useState("");
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        createMovie( {title} );
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h1>Add new Movie</h1>
+
+            <FormInput label={"Title:"} value={title} onChangeValue={setTitle} />
+            <FormInput label={"Year:"} value={year} onChangeValue={setYear} />
+            <FormInput label={"Country:"} value={country} onChangeValue={setCountry} />
+            <FormInput label={"Plot:"} value={plot} onChangeValue={setPlot} />
+        </form>
+    );
+}
---
+++ server/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import * as path from "path";
+import { MoviesApi } from "./moviesApi.js";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.json());
+
+const mongoClient = new MongoClient(process.env.MONGODB_URL);
+mongoClient.connect().then(async () => {
+  console.log("Connected to mongodb");
+  app.use(
+    "/api/movies",
+    MoviesApi(mongoClient.db(process.env.MONGODB_DATABASE || "pg6301-7"))
+  );
+});
+
+app.use(express.static("../client/dist/"));
+
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
+});
+
+const server = app.listen(process.env.PORT || 3000, () => {
+  console.log(`Started on http://localhost:${server.address().port}`);
+});
